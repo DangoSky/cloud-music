f95d93b7378350aa34bf00eaaecca455c65f67e5
DangoSky <1341514988@qq.com>
2019-01-27 16:01:50 +0800

    制作发现页面的轮播图

--- src/components/explore/header.vue
+++ src/components/explore/header.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="header">
     <img class="audio" :src="audioSRC">    
-    <input class="search" type="text" placeholder="是团子的天空幻想">
+    <input class="search" type="text" placeholder="但愿日子清净，抬头遇见的都是柔情">
     <img  class="music" src="../../assets/music.png">
   </div>
 </template> 
 
 <script>
   export default {
+    // 父组件给子组件传右上方音频icon的src
     props: ['audioSRC'],
   }
 </script>
--- src/components/explore/slideshow.vue
+++ src/components/explore/slideshow.vue
@@ -1,12 +1,26 @@
 <template>
-  <p>这是 个性推荐 组件</p>
+  <div class="carousel">
+    <transition-group tag="ul" class="carousel-picture">
+      <li v-for="item in carouselArr" :key="item"> 
+        <img :src="item">
+      </li>
+    </transition-group>
+    <div class="carousel-label">
+      <span v-for="list in carouselArr.length" :key="list"></span>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-    
+        carouselArr: [
+          'http://hbimg.b0.upaiyun.com/4280c67a64ec06aed7a7f7313f9d6fd8c1b2bec61ac52-Oo9dFa_fw658',
+          'http://hbimg.b0.upaiyun.com/79dc821529db6fa5963c0149b42a62266040fddf155f3-WSDH8y_fw658',
+          'http://hbimg.b0.upaiyun.com/72d0f8bb5c5858c31df05dd180e7f0a85a9fefd855584-7IqsX4_fw658',
+          'http://hbimg.b0.upaiyun.com/a0279d50f651fa51cafb45e4b64982a073e8f4e9140c0-vqxy8O_fw658'
+        ]
       }
     },
     methods: {
@@ -17,7 +31,7 @@
     }
   }
 </script>
-
-<style scoped>
+ 
+<style src="../../css/explore/slideshow.css" scoped>
 
 </style>
