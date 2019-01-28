802bca12a60b8d9328925f165763374a00e829dd
DangoSky <1341514988@qq.com>
2019-01-28 21:04:41 +0800

    完成发现页面主播电台的首部

--- src/components/explore/broadcasting.vue
+++ src/components/explore/broadcasting.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="broadcasting">
     <carousel :carouselArr="carouselArr"></carousel>
+    <explore-bar-broadcasting></explore-bar-broadcasting>
   </div>
 </template>
 
 <script>
   import carousel from './carousel.vue'
+  import exploreBarBroadcasting from './exploreBarBroadcasting.vue'
   export default {
     components: {
-      'carousel': carousel
+      'carousel': carousel,
+      'explore-bar-broadcasting': exploreBarBroadcasting
     },
     data() {
       return {
          carouselArr: [
-          require('../../assets/carousel1.jpg'),
-          require('../../assets/carousel2.jpg'),
-          require('../../assets/carousel3.png'),
-          require('../../assets/carousel4.jpg')
+          require('../../assets/carousel5.jpg'),
+          require('../../assets/carousel6.jpg'),
+          require('../../assets/carousel7.png'),
+          require('../../assets/carousel8.jpg')
         ]
       }
     }
---
+++ src/components/explore/exploreBarBroadcasting.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="exploreBarBroadcasting">
+    <div class="exploreBarIcon broadcastingClassify">
+      <label class="exploreBarFont">电台分类</label>
+    </div>
+    <div class="exploreBarIcon broadcastingRanking">
+      <label class="exploreBarFont">电台排行</label>
+    </div>
+    <div class="exploreBarIcon difm">
+      <label class="exploreBarFont">DI.FM</label>
+    </div>
+    <div class="exploreBarIcon xiaobingBroadcasting">
+      <label class="exploreBarFont">小冰电台</label>
+    </div>    
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style src="../../css/explore/exploreBarBroadcasting.css" scoped>
+
+</style>
