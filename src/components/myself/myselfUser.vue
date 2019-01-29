24fa4c5d3681addd28cf81b6cede04b7b7fe61a4
DangoSky <1341514988@qq.com>
2019-01-30 01:05:46 +0800

    完成 我的 页面的大致布局

--- src/components/myself/myselfUser.vue
+++ src/components/myself/myselfUser.vue
@@ -2,8 +2,9 @@
   <div class="myselfUser">
     <div class="myselfUserBox">
       <img src="../../assets/carousel1.jpg" class="myselfHeadPortrait">
-      <label>{{ userName }}</label>
-      <img src="../../assets/musicBag.png">
+      <label class="userName">{{ userName }}</label>
+      <img src="../../assets/musicBag.png" class="musicBag">
+      <label class="myselfVip">会员中心</label>
     </div>
   </div>
 </template>
