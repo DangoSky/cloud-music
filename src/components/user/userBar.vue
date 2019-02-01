533b8bb0940ae2f85582d7a3d4bf0c0e3fda1f05
DangoSky <1341514988@qq.com>
2019-02-01 15:16:58 +0800

    账号页面新消息提示

--- src/components/user/userBar.vue
+++ src/components/user/userBar.vue
@@ -7,7 +7,7 @@
       :key="item.name" 
       :style="{background: 'url(' + item.bgSrc + ') no-repeat 10px 10px white'}"
     >
-      <label style="cursor: pointer;">{{ item.name }}</label>
+      <label style="cursor: pointer;" :class="{newClass: item.haveNew}">{{ item.name }}</label>
       <img src="../../assets/rightArrow.png" class="rightArrow">
     </div>
     <p class="userFooter">退出登陆</p>
@@ -22,103 +22,122 @@
           {
             name: '我的消息',
             bgSrc: require('../../assets/message.png'),
-            haveGap: true
+            haveGap: true,
+            haveNew: true
           },
           {
             name: '我的会员',
             bgSrc: require('../../assets/vip.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '云村有票',
             bgSrc: require('../../assets/ticket.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '商城',
             bgSrc: require('../../assets/shop.png'),
-            haveGap: false
-
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '精品游戏推荐',
             bgSrc: require('../../assets/game.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '在线听歌免流量',
             bgSrc: require('../../assets/flow.png'),
-            haveGap: true
+            haveGap: true,
+            haveNew: false
           },
           {
             name: '设置',
             bgSrc: require('../../assets/setting.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '扫一扫',
             bgSrc: require('../../assets/richScan.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '个性换肤',
             bgSrc: require('../../assets/skin.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '夜间模式',
             bgSrc: require('../../assets/night.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '定时关闭',
             bgSrc: require('../../assets/timingClose.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '音乐闹钟',
             bgSrc: require('../../assets/clock.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '驾驶模式',
             bgSrc: require('../../assets/drive.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '古典专区',
             bgSrc: require('../../assets/piano.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '亲子频道',
             bgSrc: require('../../assets/family.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '小冰电台',
             bgSrc: require('../../assets/xiaobing1.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '优惠券',
             bgSrc: require('../../assets/discount.png'),
-            haveGap: true
+            haveGap: true,
+            haveNew: true
           },
           {
             name: '加入网易云音乐人',
             bgSrc: require('../../assets/music2.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '分享网易云音乐',
             bgSrc: require('../../assets/share.png'),
-            haveGap: false
+            haveGap: false,
+            haveNew: false
           },
           {
             name: '关于',
             bgSrc: require('../../assets/about.png'),
-            haveGap: true
+            haveGap: true,
+            haveNew: false
           }
         ]
       }
