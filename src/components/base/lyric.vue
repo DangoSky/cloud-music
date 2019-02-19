2602fc2877f30952c96aabb57beb44885e334e3d
DangoSky <1341514988@qq.com>
2019-02-19 23:48:11 +0800

    修复歌词滚动的尺度问题，使当前歌词保持在中间

--- src/components/base/lyric.vue
+++ src/components/base/lyric.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="lyric" @click="setShowLyric(!showLyric)">
-    <div class="lyricBox" style="transition: all 0.5s" :style="{'transform': `translateY(${offsetLen}px)`}"
-    
-    >
-      <p v-for="(item, index) in lyricArr" :key="index" :class="{'curLyric': index === currentLyric}">
+    <div class="lyricBox" style="transition: all 0.5s" :style="{'transform': `translateY(${offsetLen}px)`}">
+      <p v-if="noLyric" style="color: white">暂无歌词</p>
+      <p v-for="(item, index) in lyricArr" :key="index" :class="{'curLyric': index === currentLyric}" v-else ref="lyric">
         {{item.text}}
       </p>
     </div>
@@ -14,24 +13,43 @@
   import api from '../../api/index.js'
   import { mapState, mapMutations } from 'vuex'
   export default {
+    // 防止重新打开歌词页面时noLyric被重置
     created() {
-      this.computedOffset();
+      if(this.lyric === '暂无歌词') {
+        this.noLyric = true;
+      }
+      else {
+        this.noLyric = false;
+      }
     },
     data() {
       return {
         currentLyric: 0,
-        offsetLen: 0
+        offsetLen: 0,
+        noLyric: false
       }
     },
     methods: {
       computedOffset() {
-        for(let i=0; i<this.lyricArr.length; i++) {
-          if(this.pastTime >= this.lyricArr[i].time && this.pastTime <= this.lyricArr[i+1].time) {
-            if(!this.lyricArr[i].text) {
-              continue;
+        if(this.lyric !== '暂无歌词') {
+          for(let i=0; i<this.lyricArr.length; i++) {
+            if(this.pastTime >= this.lyricArr[i].time) {
+              // 如果满足上面一个if条件，并且i是最后一个，则直接显示，否则通过拖动点击到最后会报错
+              if((i !== this.lyricArr.length - 1 && this.pastTime <= this.lyricArr[i+1].time) || i === this.lyricArr.length - 1) {
+                // 对于是空行的歌词让它显示为上一句歌词
+                if(!this.lyricArr[i].text)  {
+                  this.currentLyric = i - 1;
+                  continue;
+                }
+                this.currentLyric = i;
+              }
+              // 根据前面每一行歌词的高度来计算平移的距离，因为有的歌词会有多行
+              let tem = 0;
+              for(let j=0; j<this.currentLyric; j++) {
+                tem += this.$refs.lyric[j].clientHeight;
+              }
+              this.offsetLen = -tem;
             }
-            this.currentLyric = i;
-            this.offsetLen =  i * (-32);
           }
         }
       },
@@ -39,23 +57,27 @@
         'setShowLyric',
         'setLyricArr',
       ]),
-
     },
     computed: {
       ...mapState([
         'showLyric',
         'lyricArr',
         'pastTime',
-        'lyricArr',
-        'lyric',
-        'draged',
-        'draging'
+        'lyric'
       ])
     },
     watch: {
       pastTime: function() {
         this.computedOffset();
       },
+      lyric: function() {
+        if(this.lyric === '暂无歌词') {
+          this.noLyric = true;
+        }
+        else {
+          this.noLyric = false;
+        }
+      }
     }
   }
 </script>
